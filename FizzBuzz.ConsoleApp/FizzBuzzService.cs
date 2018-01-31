73b46bf67b77ae7752995e04aa26cadab4c7f0ff
Thomas Alexander Vaags <thomas.vaags@gmail.com>
2018-01-31 09:04:56 +0100

    Added comments

--- FizzBuzz.ConsoleApp/FizzBuzzService.cs
+++ FizzBuzz.ConsoleApp/FizzBuzzService.cs
@@ -4,10 +4,16 @@ namespace FizzBuzz.ConsoleApp
 {
     public class FizzBuzzService : IFizzBuzzService
     {
+        /// <summary>
+        /// Determines FizzBuzz value of number.
+        /// See https://en.wikipedia.org/wiki/Fizz_buzz for details
+        /// </summary>
+        /// <param name="i">Number to be FizzBuzzed</param>
+        /// <returns>FizzBuzz value of number</returns>
         public string GetFizzBuzzValue(int i)
         {
-            var isFizz = i % 3 == 0;
-            var isBuzz = i % 5 == 0;
+            var isFizz = i % 3 == 0; // The number is divisible by 3
+            var isBuzz = i % 5 == 0; // The number is divisible by 5
 
             if (isFizz && isBuzz)
             {
@@ -27,6 +33,11 @@ namespace FizzBuzz.ConsoleApp
             return i.ToString();
         }
 
+        /// <summary>
+        /// Creates list of FizzBuzz values
+        /// </summary>
+        /// <param name="numberOfItems">Number of list items</param>
+        /// <returns>List of FizzBuzz values</returns>
         public List<string> GetFizzBuzzResults(int numberOfItems)
         {
             var results = new List<string>();
